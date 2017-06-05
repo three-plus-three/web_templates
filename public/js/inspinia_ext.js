56a25e269caa4384ab36159e74d8599617038874
meifakun <runner.mei@gmail.com>
2017-06-05 11:25:41 +0800

    remove taginputs.js

--- public/js/inspinia_ext.js
+++ public/js/inspinia_ext.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
   $.validator.defaults.ignore = ":hidden:not(.force-validate)";
   $("form.validation-required").validate();
 
-  $('.tagsinput').tagsinput({
-    tagClass: 'label label-primary'
-  });
+  // $('.tagsinput').tagsinput({
+  //  tagClass: 'label label-primary'
+  // });
 });
 
 $(document).ready(function () {
